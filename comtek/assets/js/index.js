ceea8bb25c985a274ece3c77c5258a6fef2b5ccb
dtoxd <42573173+dtoxd@users.noreply.github.com>
2018-11-21 10:41:13 +0530

    Add files via upload

---
+++ comtek/assets/js/index.js
@@ -0,0 +1,56 @@
+$(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
+  
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
+
+
+jQuery(document).ready(function ($) {
+
+
+//    featured slider
+    var swiper = new Swiper('.swiper-container', {
+//        pagination: '.swiper-pagination',
+        loop: true,
+        slidesPerView: 'auto',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30,
+        coverflow: {
+            rotate: 5,
+            stretch: 0,
+            depth: 100,
+            modifier: 3,
+            slideShadows: false
+        }
+    });
+
+
+});
